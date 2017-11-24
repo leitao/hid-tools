15a49f4e9eb43ef35455fd7bf59d01aecae1407e
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2017-11-24 18:57:29 +0100

    implement hid-replay in python
    To be able to run a generated test suite without spawning new processes,
    well, we need a python wrapper for it
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

---
+++ hid-replay.py
@@ -0,0 +1,134 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Hid tools / hid-replay.py
+#
+# Copyright (c) 2017 Benjamin Tissoires <benjamin.tissoires@gmail.com>
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from datetime import datetime, timedelta
+import select
+import sys
+import time
+import uhid
+
+
+class HIDReplay(object):
+    def __init__(self, filename):
+        self._devices = {}
+        self._devices_fd = {}
+        self.filename = filename
+        with open(filename) as f:
+            idx = 0
+            name = None
+            rdesc = None
+            phys = None
+            info = None
+            for l in f.readlines():
+                if l.startswith('D:'):
+                    idx = int(l.split(' ')[1])
+                    name = None
+                    rdesc = None
+                    phys = None
+                    info = None
+                    continue
+                if idx not in self._devices:
+                    self._devices[idx] = uhid.UHIDDevice()
+                dev = self._devices[idx]
+                if l.startswith('N:'):
+                    name = l.split(' ', 1)[1].strip()
+                    dev.name = name
+                elif l.startswith('I:'):
+                    bus, vid, pid = l.split(' ')[1:]
+                    info = [int(i, 16) for i in (bus, vid, pid)]
+                    dev.info = info
+                elif l.startswith('P:'):
+                    phys = l.split(' ', 1)[1].strip()
+                    dev.phys = phys
+                elif l.startswith('R:'):
+                    length, rdesc = l.split(' ', 2)[1:]
+                    rdesc = [int(r, 16) for r in rdesc.split()]
+                    assert int(length) == len(rdesc)
+                    dev.rdesc = rdesc
+
+        self.poll = select.poll()
+        for d in self._devices.values():
+            d.create_kernel_device()
+            self.poll.register(d.fd)
+            self._devices_fd[d.fd] = d
+
+    def process_one_event(self, timeout=None):
+        devices = self.poll.poll(timeout)
+        for fd, mask in devices:
+            dev = self._devices_fd[fd]
+            dev.process_one_event()
+        return len(devices)
+
+    def destroy(self):
+        for d in self._devices.values():
+            d.destroy()
+
+    def inject_events(self, wait_max_seconds=2):
+        t = None
+        timestamp_offset = 0
+        with open(self.filename) as f:
+            idx = 0
+            dev = None
+            if idx in self._devices:
+                dev = self._devices[idx]
+            for l in f.readlines():
+                if l.startswith('D:'):
+                    idx = int(l.split(' ')[1])
+                    dev = self._devices[idx]
+                elif l.startswith('E:'):
+                    data = l.split(' ', 1)[1]
+                    timestamp, length, data = data.split(' ', 2)
+                    timestamp = float(timestamp)
+                    data = [int(d, 16) for d in data.split()]
+                    assert len(data) == int(length)
+                    now = datetime.today()
+                    if t is None:
+                        t = now
+                        timestamp_offset = timestamp
+                    target_time = t + timedelta(seconds=timestamp - timestamp_offset)
+                    sleep = target_time - now
+                    sleep = sleep.seconds + sleep.microseconds / 1000000
+                    if sleep < 0.01:
+                        pass
+                    elif sleep < wait_max_seconds:
+                        time.sleep(sleep)
+                    else:
+                        t = now
+                        timestamp_offset = timestamp
+                        time.sleep(wait_max_seconds)
+                    dev.call_input_event(data)
+
+
+def main(argv):
+    try:
+        replay = HIDReplay(argv[0])
+        while replay.process_one_event(1000):
+            pass
+        print('Hit enter (re)start replaying the events')
+        sys.stdin.readline()
+        replay.inject_events()
+    finally:
+        replay.destroy()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ uhid.py
@@ -0,0 +1,217 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Hid tools / uhid.py
+#
+# Copyright (c) 2017 Benjamin Tissoires <benjamin.tissoires@gmail.com>
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import struct
+
+
+class UHIDUncompleteException(Exception):
+    pass
+
+
+class UHIDDevice(object):
+    __UHID_LEGACY_CREATE = 0
+    UHID_DESTROY = 1
+    UHID_START = 2
+    UHID_STOP = 3
+    UHID_OPEN = 4
+    UHID_CLOSE = 5
+    UHID_OUTPUT = 6
+    __UHID_LEGACY_OUTPUT_EV = 7
+    __UHID_LEGACY_INPUT = 8
+    UHID_GET_REPORT = 9
+    UHID_GET_REPORT_REPLY = 10
+    UHID_CREATE2 = 11
+    UHID_INPUT2 = 12
+    UHID_SET_REPORT = 13
+    UHID_SET_REPORT_REPLY = 14
+
+    def __init__(self):
+        self._name = None
+        self._phys = ''
+        self._rdesc = None
+        self._info = None
+        self._fd = os.open('/dev/uhid', os.O_RDWR)
+        self._set_report_fun = self._set_report
+        self._get_report_fun = self._get_report
+        self._output_report_fun = self._output_report
+        self.opened = False
+
+    @property
+    def fd(self):
+        return self._fd
+
+    @property
+    def rdesc(self):
+        return self._rdesc
+
+    @rdesc.setter
+    def rdesc(self, rdesc):
+        self._rdesc = rdesc
+
+    @property
+    def phys(self):
+        return self._phys
+
+    @phys.setter
+    def phys(self, phys):
+        self._phys = phys
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def info(self):
+        return self._info
+
+    @info.setter
+    def info(self, info):
+        self._info = info
+
+    @property
+    def bus(self):
+        return self._info[0]
+
+    @property
+    def vid(self):
+        return self._info[1]
+
+    @property
+    def pid(self):
+        return self._info[2]
+
+    @property
+    def set_report(self):
+        return self._set_report_fun
+
+    @set_report.setter
+    def set_report(self, set_report):
+        self._set_report_fun = set_report
+
+    def call_set_report(self, req, err):
+        buf = struct.pack('< L L H',
+                          UHIDDevice.UHID_SET_REPORT_REPLY,
+                          req,
+                          err)
+        os.write(self._fd, buf)
+
+    def _set_report(self, req, rnum, rtype, size, data):
+        self.call_set_report(req, 1)
+
+    @property
+    def get_report(self):
+        return self._get_report_fun
+
+    @get_report.setter
+    def get_report(self, get_report):
+        self._get_report_fun = get_report
+
+    def call_get_report(self, req, data, err):
+        data = bytes(data)
+        buf = struct.pack('< L L H H 4096s',
+                          UHIDDevice.UHID_GET_REPORT_REPLY,
+                          req,
+                          err,
+                          len(data),
+                          data)
+        os.write(self._fd, buf)
+
+    def _get_report(self, req, rnum, rtype):
+        self.call_get_report(req, [], 1)
+
+    @property
+    def output_report(self):
+        return self._output_report_fun
+
+    @output_report.setter
+    def output_report(self, output_report):
+        self._output_report_fun = output_report
+
+    def _output_report(self, data, size, rtype):
+        pass
+
+    def call_input_event(self, data):
+        data = bytes(data)
+        buf = struct.pack('< L H 4096s',
+                          UHIDDevice.UHID_INPUT2,
+                          len(data),
+                          data)
+        os.write(self._fd, buf)
+
+    def create_kernel_device(self):
+        if (self._name is None or
+           self._rdesc is None or
+           self._info is None):
+            raise UHIDUncompleteException("missing uhid initialization")
+
+        buf = struct.pack('< L 128s 64s 64s H H L L L L 4096s',
+                          UHIDDevice.UHID_CREATE2,
+                          bytes(self._name, 'utf-8'),  # name
+                          bytes(self._phys, 'utf-8'),  # phys
+                          b'',  # uniq
+                          len(self._rdesc),  # rd_size
+                          self.bus,  # bus
+                          self.vid,  # vendor
+                          self.pid,  # product
+                          0,  # version
+                          0,  # country
+                          bytes(self._rdesc))  # rd_data[HID_MAX_DESCRIPTOR_SIZE]
+
+        n = os.write(self._fd, buf)
+        assert n == len(buf)
+
+    def destroy(self):
+        buf = struct.pack('< L',
+                          UHIDDevice.UHID_DESTROY)
+        os.write(self._fd, buf)
+
+    def process_one_event(self):
+        buf = os.read(self._fd, 4380)
+        assert len(buf) == 4380
+        evtype = struct.unpack_from('< L', buf)[0]
+        if evtype == UHIDDevice.UHID_START:
+            ev, flags = struct.unpack_from('< L Q', buf)
+        elif evtype == UHIDDevice.UHID_OPEN:
+            self.opened = True
+            print('open')
+        elif evtype == UHIDDevice.UHID_STOP:
+            print('stop')
+        elif evtype == UHIDDevice.UHID_CLOSE:
+            self.opened = False
+            print('close')
+        elif evtype == UHIDDevice.UHID_SET_REPORT:
+            ev, req, rnum, rtype, size, data = struct.unpack_from('< L L B B H 4096s', buf)
+            self.set_report(req, rnum, rtype, size, data)
+            print('set report', req, rtype, size, data[:size])
+        elif evtype == UHIDDevice.UHID_GET_REPORT:
+            ev, req, rnum, rtype = struct.unpack_from('< L L B B', buf)
+            self.get_report(req, rnum, rtype)
+            print('get report', req, rnum, rtype)
+        elif evtype == UHIDDevice.UHID_OUTPUT:
+            ev, data, size, rtype = struct.unpack_from('< L 4096s H B', buf)
+            self.output_report(data, size, rtype)
+            print('output', data[:size], size, rtype)
