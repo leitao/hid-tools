3faab872548e046487356d732769eccfa5a893b9
Peter Hutterer <peter.hutterer@who-t.net>
2018-11-14 10:55:25 +1000

    hid: fix typo
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- hidtools/hid.py
+++ hidtools/hid.py
@@ -110,7 +110,7 @@ inv_collections = dict([(v, k) for k, v in collections.items()])
 
 
 def twos_comp(val, bits):
-    """compute the 2's compliment of int value val"""
+    """compute the 2's complement of int value val"""
     if (val & (1 << (bits - 1))) != 0:
         val = val - (1 << bits)
     return val
