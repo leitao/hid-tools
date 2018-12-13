29bdfe5631eaa087f6bfc38045da470eb8fefb13
Peter Hutterer <peter.hutterer@who-t.net>
2018-12-14 09:57:35 +1000

    setup.py: generate man pages at install time
    To run a command on install, we need to override the install class with our
    own. And we want to convert md to man through pandoc, so let's use pypandoc
    for that.
    
    Unfortunately, pypandoc 1.4 (latest release) spews helpful install
    instructions for pandoc to stderr when it fails to call pandoc, not something
    we want. So let's redirect stderr for the first check if it works and bail out
    accordingly.
    
    If we fail, we just drop the man pages, they're not that important.
    pandoc requires the haskell toolchain installed, there will be plenty of cases
    where just installing without man pages is preferable.
    
    It's unclear if self.distribution in the install subclass is considered public
    API or will break at some point. The documentation is lacking.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- setup.py
+++ setup.py
@@ -1,6 +1,65 @@
 #!/usr/bin/python3
 
+import os
+import sys
+import io
 from setuptools import setup
+from setuptools.command.install import install
+
+
+class ManPageGenerator(install):
+    def run(self):
+        man_pages = self.pandocify()
+        if man_pages:
+            entry = (os.path.join(self.prefix, 'share', 'man', 'man1'),
+                     man_pages)
+            self.distribution.data_files.append(entry)
+        super().run()
+
+    def pandocify(self):
+        try:
+            import pypandoc
+            # pypandoc spews install instructions to stderr when it can't find
+            # pandoc. So we redirect stderr and restore it with a saner
+            # message.
+            old_stderr = sys.stderr
+            pandoc_found = True
+            try:
+                with io.StringIO() as s:
+                    sys.stderr = s
+                    pypandoc.get_pandoc_version()
+            except OSError:
+                pandoc_found = False
+            finally:
+                sys.stderr = old_stderr
+
+            if not pandoc_found:
+                print('*****************************************************\n'
+                      'Pandoc man page conversion failed, skipping man pages\n'
+                      '*****************************************************\n',
+                      file=sys.stderr)
+                return None
+
+            # now do the actual conversion
+            here = os.path.dirname(__file__) or '.'
+            mandir = os.path.join(here, 'man')
+            man_pages = []
+            for f in os.listdir(mandir):
+                if f.endswith('.md'):
+                    path = os.path.join(mandir, f)
+                    outfile = f'{path[:-3]}.1'
+                    pypandoc.convert_file(path, 'man',
+                                          outputfile=outfile,
+                                          extra_args=['-s'])
+                    man_pages.append(outfile)
+        except ModuleNotFoundError:
+            print('*********************************************\n'
+                  'Module pypandoc not found, skipping man pages\n'
+                  '*********************************************\n',
+                  file=sys.stderr)
+            return None
+        return man_pages
+
 
 setup(name='hid-tools',
       version='0.1',
@@ -25,10 +84,11 @@ setup(name='hid-tools',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6'
       ],
-      data_files=[('/usr/share/man/man1', ['man/hid-recorder.1',
-                                           'man/hid-replay.1',
-                                           'man/hid-decode.1'])],
+      data_files=[],  # man pages are added on success
       python_requires='>=3.6',
       include_package_data=True,
-      install_requires=['parse', 'pyudev']
+      install_requires=['parse', 'pyudev'],
+      cmdclass=dict(
+        install=ManPageGenerator,
+      )
       )
