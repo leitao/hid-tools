839086cd84374fa10349f6cc29be2e647d6687c4
Peter Hutterer <peter.hutterer@who-t.net>
2018-12-13 11:39:04 +1000

    record: handle the 'no devices' case gracefully
    Instead of crashing later
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- hidtools/cli/record.py
+++ hidtools/cli/record.py
@@ -36,6 +36,10 @@ def list_devices():
             d = HidrawDevice(f)
             devices[int(fname[6:])] = d.name
 
+    if not devices:
+         print('No devices found', file=sys.stderr)
+         sys.exit(1)
+
     print('Available devices:', file=outfile)
     for num, name in sorted(devices.items()):
         print(f'/dev/hidraw{num}:	{name}', file=outfile)
